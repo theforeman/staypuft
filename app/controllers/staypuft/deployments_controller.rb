095f5df8c2cc71294f8aa98af92be50f1b68673b
Scott Seago <sseago@redhat.com>
2014-04-22 12:09:59 -0700

    Revert "Merge pull request #87 from pitr-ch/master"
    This reverts commit c3a3d9d9b6c708bf9d1d17be9fea6a70c42a8ee2, reversing
    changes made to 15decb65e5769d99edf424a1b9bfffd4d4865a52.

--- app/controllers/staypuft/deployments_controller.rb
+++ app/controllers/staypuft/deployments_controller.rb
@@ -64,11 +64,10 @@ module Staypuft
       hosts_to_remove = assigned_hosts - targeted_hosts
 
       hosts_to_assign.each do |discovered_host|
-        original_type = discovered_host.type
-        host          = discovered_host.becomes(::Host::Managed)
-        host.type     = 'Host::Managed'
-        host.managed  = true
-        host.build    = true
+        host         = discovered_host.becomes(::Host::Managed)
+        host.type    = 'Host::Managed'
+        host.managed = true
+        host.build   = true
 
         host.hostgroup   = hostgroup
         # set discovery environment to keep booting discovery image
@@ -85,12 +84,7 @@ module Staypuft
         # FIXME this is definitely ugly, needs to be properly fixed
         discovered_host.update_column :type, 'Host::Managed'
 
-        begin
-          host.save!
-        rescue => e
-          discovered_host.update_column :type, original_type
-          raise e
-        end
+        host.save!
       end
 
       hosts_to_remove.each do |host|
