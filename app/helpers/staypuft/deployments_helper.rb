74b0a5bb877175b2c865786f66b4ce6fb6454b59
Jason E. Rist <jrist@redhat.com>
2014-04-22 10:55:52 -0600

    Remove raw, mark original method return html_safe

--- app/helpers/staypuft/deployments_helper.rb
+++ app/helpers/staypuft/deployments_helper.rb
@@ -15,10 +15,10 @@ module Staypuft
 
     def alert_if_deployed
       if @deployment.hosts.any?(&:open_stack_deployed?)
-        alert :class => 'alert-warning',
+        (alert :class => 'alert-warning',
               :text => _('Machines are already deployed with this configuration. Changing the configuration parameters
                           is unsupported and may result in an unusable configuration. <br/>Please proceed with caution.'),
-              :close => false
+              :close => false).html_safe
       end
     end
 
