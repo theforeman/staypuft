5afd6559719f86c2cd482e1494ef98e5e5faf5b7
Marek Hulan <mhulan@redhat.com>
2014-05-22 16:41:47 +0200

    Hide password even with older deface

--- app/overrides/hide_subscription_manager_passwords.rb
+++ app/overrides/hide_subscription_manager_passwords.rb
@@ -1,7 +1,13 @@
+# older deface requires code prefix for erb tags
+if Gem.loaded_specs['deface'].version >= Gem::Version.new('1.0.0')
+  erb_tag = 'erb[loud]'
+else
+  erb_tag = 'code[erb-loud]'
+end
+
 Deface::Override.new(:virtual_path => "common_parameters/_inherited_parameters",
                      :name => "hide_subscription_manager_passwords",
-                     :surround => 'td.col-md-7 erb[loud]:contains("parameter_value_field inherited_parameters[name]")',
+                     :surround => 'td.col-md-7 ' + erb_tag + ':contains("parameter_value_field inherited_parameters[name]")',
                      :original => '<%= parameter_value_field inherited_parameters[name] %>',
                      :text => "<% if name == 'subscription_manager_password' %>*****<% else %><%= render_original %><% end %>"
                      )
-
