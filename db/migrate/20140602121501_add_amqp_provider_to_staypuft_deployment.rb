089aaf3e9ca1035f89c4229ce88c04f838c2152a
Petr Chalupa <git@pitr.ch>
2014-06-04 10:21:21 +0200

    Do not use real AR models in migrations

--- db/migrate/20140602121501_add_amqp_provider_to_staypuft_deployment.rb
+++ db/migrate/20140602121501_add_amqp_provider_to_staypuft_deployment.rb
@@ -1,13 +1,13 @@
 class AddAmqpProviderToStaypuftDeployment < ActiveRecord::Migration
+  Deployment = Class.new(ActiveRecord::Base) do
+    self.table_name = 'staypuft_deployments'
+  end
+
   def change
     add_column :staypuft_deployments, :amqp_provider, :string
 
-    Staypuft::Deployment.all.each do |deployment|
-      deployment.amqp_provider = 'rabbitmq'
-      deployment.save!
-    end
+    Deployment.update_all :amqp_provider => 'rabbitmq'
 
     change_column :staypuft_deployments, :amqp_provider, :string, :null => false
-
   end
 end
