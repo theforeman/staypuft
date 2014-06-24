075b70dd3d215ce8294bb29271d9a5df1557124d
Petr Chalupa <git@pitr.ch>
2014-06-24 12:02:56 +0200

    Update Operationsystem based on platform param
    fix update_hostgroup_list and other updates to be called automatically

--- app/models/staypuft/deployment_role_hostgroup.rb
+++ app/models/staypuft/deployment_role_hostgroup.rb
@@ -3,7 +3,7 @@ module Staypuft
     attr_accessible :deployment, :deployment_id, :hostgroup, :hostgroup_id, :role, :role_id, :deploy_order
 
     belongs_to :deployment
-    belongs_to :hostgroup
+    belongs_to :hostgroup, dependent: :destroy
     belongs_to :role
     has_many :services, :through => :role
 
