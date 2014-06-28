0c30ae26aee7db15ccbb8b233a7541d1ee4faadc
Scott Seago <sseago@redhat.com>
2014-06-27 21:48:54 -0700

    remove no-longer-needed UI elements

--- app/models/staypuft/deployment/neutron_service.rb
+++ app/models/staypuft/deployment/neutron_service.rb
@@ -9,6 +9,7 @@ module Staypuft
     param_attr :network_segmentation, :tenant_vlan_ranges, :networker_tenant_interface,
                :use_external_interface, :external_interface_name, :compute_tenant_interface
 
+
     module NetworkSegmentation
       VXLAN  = 'vxlan'
       GRE    = 'gre'
@@ -53,23 +54,6 @@ module Staypuft
               :presence     => true,
               :if           => :use_external_interface
     # TODO: interface name format validation
-
-    module UseVlanForExternalNetwork
-      # TODO: no text here; remove module if final UI still leaves blank
-    end
-    validates :use_vlan_for_external_network,
-              :inclusion => { in: [true, false] },
-              :if        => :use_external_interface
-
-    module VlanIdForExternalNetwork
-      HUMAN        = N_('Use VLAN ID')
-      HUMAN_AFTER  = N_('for External Network')
-    end
-    validates :vlan_id_for_external_network,
-              :presence     => true,
-              :if           => :external_network_vlan?
-    # TODO: VLAN ID format/value validation
-
 
     module ComputeTenantInterface
       HUMAN        = N_('Which interface to use for tenant networks:')
