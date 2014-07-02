4822ae7df492a49b1382abbf144389f27cd21ceb
Petr Chalupa <git@pitr.ch>
2014-07-02 11:02:24 +0200

    Add fixme

--- app/controllers/staypuft/deployments_controller.rb
+++ app/controllers/staypuft/deployments_controller.rb
@@ -24,6 +24,7 @@ module Staypuft
       @service_hostgroup_map = @deployment.services_hostgroup_map
     end
 
+    # FIXME: missing update action, there is no way how to submit the edited params
     def edit
       @deployment            = Deployment.find(params[:id])
       @service_hostgroup_map = @deployment.services_hostgroup_map
