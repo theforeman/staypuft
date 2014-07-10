158230b65f66b235bc480d9742776bb923b1b547
Martyn Taylor <mtaylor@redhat.com>
2014-07-10 15:47:13 +0100

    Add swift_admin password and rework password lists

--- app/models/staypuft/deployment/passwords.rb
+++ app/models/staypuft/deployment/passwords.rb
@@ -1,19 +1,18 @@
 module Staypuft
   class Deployment::Passwords < Deployment::AbstractParamScope
-    PASSWORD_LIST = :admin, :ceilometer_user, :cinder_db, :cinder_user,
-        :glance_db, :glance_user, :heat_db, :heat_user, :heat_cfn_user, :mysql_root,
-        :keystone_db, :keystone_user, :neutron_db, :neutron_user, :nova_db, :nova_user,
-        :swift_admin, :swift_user, :amqp, :amqp_nssdb, :keystone_admin_token,
-        :ceilometer_metering_secret, :heat_auth_encrypt_key, :horizon_secret_key,
-        :swift_shared_secret, :neutron_metadata_proxy_secret
-
-    OTHER_ATTRS_LIST = :mode, :single_password
 
     USER_SERVICES_PASSWORDS = :admin, :ceilometer_user, :cinder_user, :glance_user, :heat_user,
-        :heat_cfn_user, :keystone_user, :neutron_user, :nova_user, :swift_user, :amqp
+        :heat_cfn_user, :keystone_user, :neutron_user, :nova_user, :swift_user, :swift_admin, :amqp
 
     DB_SERVICES_PASSWORDS = :cinder_db, :glance_db, :heat_db, :mysql_root, :keystone_db,
         :neutron_db, :nova_db, :amqp_nssdb
+
+    OTHER_PASSWORDS = :keystone_admin_token, :ceilometer_metering_secret, :heat_auth_encrypt_key,
+        :horizon_secret_key, :swift_shared_secret, :neutron_metadata_proxy_secret
+
+    PASSWORD_LIST = USER_SERVICES_PASSWORDS + DB_SERVICES_PASSWORDS + OTHER_PASSWORDS
+
+    OTHER_ATTRS_LIST = :mode, :single_password
 
     def self.param_scope
       'passwords'
