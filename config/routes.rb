1e6537cb545897793880f3a79b182ad0b8c29737
Martyn Taylor <mtaylor@redhat.com>
2014-07-16 10:47:33 +0100

    Removed hostgroup param from show deployment URL

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,6 @@ Rails.application.routes.draw do
         get 'auto_complete_search'
       end
       member do
-        match '/hostgroup/:hostgroup_id',
-              :to => 'deployments#show',
-              :as => :show_with_hostgroup_selected, :method => :get
         post 'deploy'
         get 'populate'
         get 'summary'
