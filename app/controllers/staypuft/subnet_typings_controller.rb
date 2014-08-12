5c5ff2b0860b7d65c98676c80fdb23cfdd993730
Marek Hulan <mhulan@redhat.com>
2014-08-12 13:38:47 +0200

    Display warning when touching subnets
    If subnet has been already assigned to NIC in particular deployment
    we display warning flash.

--- app/controllers/staypuft/subnet_typings_controller.rb
+++ app/controllers/staypuft/subnet_typings_controller.rb
@@ -4,6 +4,7 @@ module Staypuft
       @deployment = Deployment.find(params[:deployment_id])
       @subnet_type = SubnetType.find(params[:subnet_type_id])
       @subnet = Subnet.find(params[:subnet_id])
+      check_for_existing_assignments
       @subnet_typing = @deployment.subnet_typings.new(:subnet_id => @subnet.id, :subnet_type_id => @subnet_type.id)
       @saved = @subnet_typing.save
     end
@@ -13,6 +14,7 @@ module Staypuft
       @deployment = @subnet_typing.deployment
       @subnet_type = @subnet_typing.subnet_type
       @subnet = Subnet.find(params[:subnet_id])
+      check_for_existing_assignments
       @subnet_typing.subnet = @subnet
       @saved = @subnet_typing.save
     end
@@ -20,8 +22,21 @@ module Staypuft
     def destroy
       @subnet_typing = SubnetTyping.find(params[:id])
       @deployment = @subnet_typing.deployment
+      @subnet = @subnet_typing.subnet
+      check_for_existing_assignments
       @subnet_type = @subnet_typing.subnet_type
       @destroyed = @subnet_typing.destroy
+    end
+
+    private
+
+    def check_for_existing_assignments
+      existing = @deployment.hosts.includes(:interfaces).any? do |h|
+        h.subnet_id == @subnet.id || h.interfaces.any? { |i| i.subnet_id == @subnet.id }
+      end
+      if existing
+        @warn = _('Some hosts interfaces were already assigned to this subnet, check interface assignment before deploying!')
+      end
     end
   end
 end
