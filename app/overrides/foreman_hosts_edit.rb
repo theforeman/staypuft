32d56a95dd24911598756c2d2a24e32689df54e1
Brad P. Crochet <brad@redhat.com>
2014-10-02 12:24:01 -0400

    BZ# 1128282 - Remove descendant hostgroups from the foreman host form
    Users should not assign hosts directly to the deployment hostgroups.
    This removes those choices from the host edit form.

--- app/overrides/foreman_hosts_edit.rb
+++ app/overrides/foreman_hosts_edit.rb
@@ -22,3 +22,16 @@ Deface::Override.new(:virtual_path => "hosts/_unattended",
                      :insert_after => "#network",
                      :text => "<div class='tab-pane' id='fencing'><%= render 'hosts/fencing', :host => @host, :f => f %></div>"
                      )
+
+Deface::Override.new(:virtual_path => "hosts/_form",
+                     :name => "remove_openstack_hostgroups",
+                     :replace => "#{erb_tag}:contains(':hostgroup_id')",
+                     :original => '04903c858c6f96ed5c015cac5960e02708d8fea8'
+                     ) do
+"       <%=  select_f f, :hostgroup_id, accessible_hostgroups.reject { |hg| hg.to_label =~ /#{Setting[:base_hostgroup]}\\/.*/ }, :id, :to_label,
+          { :include_blank => true},
+          { :onchange => 'hostgroup_changed(this);', :'data-host-id' => @host.id,
+            :'data-url' => (@host.new_record? || @host.type_changed?) ? process_hostgroup_hosts_path : hostgroup_or_environment_selected_hosts_path,
+            :help_inline => :indicator } %>
+"
+end
