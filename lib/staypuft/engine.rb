b93f34be24be7dca76a92176e8bc5f7996e92a3a
Brad P. Crochet <brad@redhat.com>
2014-10-02 13:28:16 -0400

    Extend the foreman API to disallow setting the hostgroup via the API

--- lib/staypuft/engine.rb
+++ lib/staypuft/engine.rb
@@ -42,6 +42,8 @@ module Staypuft
       ::LookupKey.send :include, Staypuft::Concerns::LookupKeyExtensions
       ::Host::Managed.send :include, Staypuft::Concerns::HostFencingExtensions
       ::Nic::Base.send :include, Staypuft::Concerns::NicFencingExtensions
+      ::Api::V1::HostsController.send :include, Staypuft::Concerns::HostsApiExtensions
+      ::Api::V2::HostsController.send :include, Staypuft::Concerns::HostsApiExtensions
 
       # preload all the Foreman's lib files but only in production
       if Rails.env.production?
