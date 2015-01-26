5e24cb2d646a493d4ecc75c15749f683608a3dc1
Brad P. Crochet <brad@redhat.com>
2015-01-26 11:37:38 -0500

    Revert "Enable NetApp driver for Cinder"

--- app/models/staypuft/deployment/cinder_service.rb
+++ app/models/staypuft/deployment/cinder_service.rb
@@ -5,12 +5,11 @@ module Staypuft
       'cinder'
     end
 
-    BACKEND_TYPE_PARAMS = :backend_eqlx, :backend_nfs, :backend_lvm, :backend_ceph, :backend_netapp
+    BACKEND_TYPE_PARAMS = :backend_eqlx, :backend_nfs, :backend_lvm, :backend_ceph
     BACKEND_PARAMS = :nfs_uri, :rbd_secret_uuid
 
     param_attr *BACKEND_TYPE_PARAMS, *BACKEND_PARAMS
     param_attr_array :eqlxs => Equallogic
-    param_attr_array :netapps => Netapp
 
     after_save :set_lvm_ptable
 
@@ -19,12 +18,10 @@ module Staypuft
       NFS        = 'nfs'
       CEPH       = 'ceph'
       EQUALLOGIC = 'equallogic'
-      NETAPP     = 'netapp'
       LABELS     = { LVM        => N_('LVM'),
                      NFS        => N_('NFS'),
                      CEPH       => N_('Ceph'),
-                     EQUALLOGIC => N_('EqualLogic'),
-                     NETAPP     => N_('NetApp') }
+                     EQUALLOGIC => N_('EqualLogic') }
       TYPES      = LABELS.keys
       HUMAN      = N_('Choose Driver Backend')
     end
@@ -65,70 +62,12 @@ module Staypuft
     validate :equallogic_backends,
              :if          => :equallogic_backend?
 
-    module NetappHostname
-      HUMAN   = N_('Hostname:')
-    end
-    module NetappLogin
-      HUMAN   = N_('Login:')
-    end
-    module NetappPassword
-      HUMAN   = N_('Password:')
-    end
-    module NetappServerPort
-      HUMAN   = N_('Server Port:')
-    end
-    module NetappStorageFamily
-      HUMAN   = N_('Storage Family:')
-    end
-    module NetappTransportType
-      HUMAN   = N_('Transport Type:')
-    end
-    module NetappStorageProtocol
-      HUMAN   = N_('Storage Protocol:')
-    end
-    module NetappNfsShares
-      HUMAN   = N_('NFS Shares:')
-    end
-    module NetappNfsSharesConfig
-      HUMAN   = N_('NFS Shares Config:')
-    end
-    module NetappVolumeList
-      HUMAN   = N_('Volume List:')
-    end
-    module NetappVfiler
-      HUMAN   = N_('vFiler:')
-    end
-    module NetappVserver
-      HUMAN   = N_('Storage Virtual Machine (SVM):')
-    end
-    module NetappControllerIps
-      HUMAN   = N_('Controller IPs:')
-    end
-    module NetappSaPassword
-      HUMAN   = N_('SA Password:')
-    end
-    module NetappStoragePools
-      HUMAN   = N_('Storage Pools:')
-    end
-
-    validates :netapps,
-              :presence   => true,
-              :if         => :netapp_backend?
-    validate :netapp_backends,
-             :if          => :netapp_backend?
-
     class Jail < Safemode::Jail
-      allow :lvm_backend?, :nfs_backend?, :ceph_backend?, :equallogic_backend?, :netapp_backend?,
+      allow :lvm_backend?, :nfs_backend?, :ceph_backend?, :equallogic_backend?,
         :multiple_backends?, :rbd_secret_uuid, :nfs_uri, :eqlxs, :eqlxs_attributes=,
         :compute_eqlx_san_ips, :compute_eqlx_san_logins, :compute_eqlx_san_passwords,
         :compute_eqlx_group_names, :compute_eqlx_pools, :compute_eqlx_thin_provision,
-        :compute_eqlx_use_chap, :compute_eqlx_chap_logins, :compute_eqlx_chap_passwords,
-        :netapps, :compute_netapp_hostnames, :compute_netapp_logins, :compute_netapp_passwords,
-        :compute_netapp_server_ports, :compute_netapp_storage_families,
-        :compute_netapp_transport_types, :compute_netapp_storage_protocolss,
-        :compute_netapp_nfs_shares, :compute_netapp_nfs_shares_configs, :compute_netapp_volume_list,
-        :compute_netapp_vfilers, :compute_netapp_vservers, :compute_netapp_controller_ips,
-        :compute_netapp_sa_passwords, :compute_netapp_storage_pools
+        :compute_eqlx_use_chap, :compute_eqlx_chap_logins, :compute_eqlx_chap_passwords
     end
 
     def set_defaults
@@ -136,7 +75,6 @@ module Staypuft
       self.backend_ceph = "false"
       self.backend_nfs = "false"
       self.backend_eqlx = "false"
-      self.backend_netapp = "false"
       self.rbd_secret_uuid = SecureRandom.uuid
     end
 
@@ -161,13 +99,8 @@ module Staypuft
       self.backend_eqlx == "true"
     end
 
-    def netapp_backend?
-      self.backend_netapp == "true"
-    end
-
     def multiple_backends?
       (equallogic_backend? and self.eqlxs.length > 1) or
-        (netapp_backend? and self.netapps.length > 1) or
         BACKEND_TYPE_PARAMS.select { |type| send(type.to_s) == "true" }.length > 1
     end
 
@@ -182,8 +115,7 @@ module Staypuft
       { "backend_lvm" => backend_lvm, "backend_ceph" => backend_ceph,
         "backend_nfs" => backend_nfs, "backend_eqlx" => backend_eqlx,
         "nfs_uri" => nfs_uri, "rbd_secret_uuid" => rbd_secret_uuid,
-        "eqlxs" => self.eqlxs, "backend_netapp" => backend_netapp,
-        "netapps" => self.netapps }
+        "eqlxs" => self.eqlxs }
     end
 
     def lvm_ptable
@@ -227,12 +159,6 @@ module Staypuft
 
     def equallogic_backends
       unless self.eqlxs.all? { |item| item.valid? }
-        errors.add :base, _("Please fix the problems in selected backends")
-      end
-    end
-
-    def netapp_backends
-      unless self.netapps.all? { |item| item.valid? }
         errors.add :base, _("Please fix the problems in selected backends")
       end
     end
